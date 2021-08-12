c24829164ff9610e8b293b227741ad578b3ab3f2
Jonathan Cai <caimjonathan@gmail.com>
2021-08-12 03:07:25 -0700

    add new artists

--- lib/artists.ts
+++ lib/artists.ts
@@ -4,8 +4,16 @@ interface IArtist {
 }
 
 const artists: IArtist[] = [
+  { name: 'Drake', slug: 'drake' },
+  { name: 'Kanye West', slug: 'kanye-west' },
+  { name: 'J. Cole', slug: 'j-cole' },
+  { name: 'Jay Z', slug: 'jay-z' },
   { name: 'Mac Miller', slug: 'mac-miller' },
   { name: 'Lil Baby', slug: 'lil-baby' },
+  { name: 'Gunna', slug: 'gunna' },
+  { name: 'Polo G', slug: 'polo-g' },
+  { name: 'Juice WRLD', slug: 'juice-wrld' },
+  { name: 'The Kid Laroi', slug: 'the-kid-laroi' },
 ]
 
 const artistData = artists.reduce((arr, artist) => {
